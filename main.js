c8f1d6f8dfa5f5c4947d2b9f9cddaebdf86a2175
yclee7 <77018121+yclee7@users.noreply.github.com>
2021-01-06 14:30:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 console.log('Hello World111hooo1111111');
-console.log('Hello World22222 +  kkkkkkkkkkkkk');
+console.log('Hello World22222 +  kkkkkkkkkkkkk+ uuuuuuuuuuuuu');
 
 
 let a = "bbbbstric test합니다.";
